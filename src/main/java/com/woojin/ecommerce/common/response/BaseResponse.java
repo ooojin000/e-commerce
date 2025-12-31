20beaa9f87cbee8653fce71741a35568e1ca64a5
woojin <ooojin523@gmail.com>
2025-12-31 15:12:30 +0900

    feat: Swagger 설정

--- src/main/java/com/woojin/ecommerce/common/response/BaseResponse.java
+++ src/main/java/com/woojin/ecommerce/common/response/BaseResponse.java
@@ -1,15 +1,24 @@
 package com.woojin.ecommerce.common.response;
 
 import com.woojin.ecommerce.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "API 기본 응답 형식")
 public class BaseResponse<T> {
+    @Schema(description = "응답 성공 여부")
     private boolean success;
+
+    @Schema(description = "응답 코드")
     private String code;
+
+    @Schema(description = "응답 메시지")
     private String message;
+
+    @Schema(description = "실제 데이터 응답")
     private T data;
 
     public static <T> BaseResponse<T> ok(T data) {
--- src/main/java/com/woojin/ecommerce/dto/OrderListResponse.java
+++ src/main/java/com/woojin/ecommerce/dto/OrderListResponse.java
@@ -1,6 +1,7 @@
 package com.woojin.ecommerce.dto;
 
 import com.woojin.ecommerce.entity.Order;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "주문 내역 응답 DTO")
 public class OrderListResponse {
     private Long id;
     private Long userId;
